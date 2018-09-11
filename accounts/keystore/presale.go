fb9bb16ba8b66d9ae366b4545da748e4fde0aeda
wuqiong <kakushao@163.com>
2018-09-11 20:21:11 +0800

    add 4 files, and add an implementation of backend

---
+++ accounts/keystore/presale.go
@@ -0,0 +1,18 @@
+package keystore
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func aesCTRXOR(key, inText, iv []byte) ([]byte, error) {
+	// AES-128 is selected due to size of encryptKey.
+	aesBlock, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	stream := cipher.NewCTR(aesBlock, iv)
+	outText := make([]byte, len(inText))
+	stream.XORKeyStream(outText, inText)
+	return outText, err
+}
